acdbf2d14b2f4ae17cfee255e322b0c4594110e9
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2023-10-02 21:18:35 -0700

    Add dialog

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,22 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import TrackerMap from './components/TrackerMap';
-import AddTrackerForm from './components/AddTrackerForm';
-import TrackerList from './components/TrackerList';
+import TrackerListDialog from './components/TrackerListDialog';
+import AddTrackerDialog from './components/AddTrackerDialog';
 
 function App() {
+  const [isTrackerListVisible, setTrackerListVisible] = useState(false);
+  const [isAddTrackerVisible, setAddTrackerVisible] = useState(false);
+
   return (
     <div className="App">
-      <AddTrackerForm />
-      <TrackerList />
+      <button onClick={() => setTrackerListVisible(true)}>Trackers</button>
+      {isTrackerListVisible && (
+        <TrackerListDialog onClose={() => setTrackerListVisible(false)} onAdd={() => setAddTrackerVisible(true)} />
+      )}
+      {isAddTrackerVisible && (
+        <AddTrackerDialog onClose={() => setAddTrackerVisible(false)} />
+      )}
       <TrackerMap />
     </div>
   );
