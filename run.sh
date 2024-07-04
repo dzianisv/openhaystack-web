e9b5165edb7783a33426c0d20cf487305ef508de
Dzianis <2119348+dzianisv@users.noreply.github.com>
2024-07-04 14:45:01 +0300

    Support python3.12

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@ export PIPENV_VENV_IN_PROJECT=1
 
 cd "$(dirname $0)"
 
+if [ ! -d openhaybike/.git ]; then
+    git submodule update --init --recursive
+fi
+
 if ! command -v pipenv 2>&1 >/dev/null; then
     pip3 install pipenv
 fi
@@ -10,5 +14,8 @@ fi
 if [[ ! -d .venv ]]; then
     pipenv install --skip-lock
 fi
+
+# https://stackoverflow.com/questions/77232001/python-eel-module-unable-to-use-import-bottle-ext-websocket-as-wbs-modulenotfoun
+pipenv run pip install auto-py-to-exe --upgrade --force-reinstall
 
 exec pipenv run python3 ./app.py
